24a5e532f4fd0823b8afc41e23c7b9c9353a697a
VarenytsiaMykhailo <l2fonat@bk.ru>
2019-09-25 19:07:45 +0300

    added Stepik/002_1.5.16 task, updated basicSyntax

--- basicSyntax.go
+++ basicSyntax.go
@@ -1,5 +1,12 @@
 package main
 
+//объявление нескольких переменных в одном блоке. Т.к. они не в функции, то являются глобальными переменными уровня пакета
+var (
+	m1 int = 12
+	m2 = "string"
+	m3 = 23
+)
+
 func main() {
 
 	//целые числа
@@ -18,6 +25,12 @@ func main() {
 	//return
 
 
+	//комплексные числа
+	var z complex64 = 2 + 3i //существуют complex64 и complex128
+	println("complex number:", z)
+	//return
+
+
 	//булевые переменные
 	var b = true
 	println("bool:", b)
@@ -31,6 +44,15 @@ func main() {
 	//return
 
 
+	//значения по-умолчанию
+	var defaultInt int
+	var defaultFloat float32
+	var defaultString string
+	var defaultBool bool
+	println("default values:", defaultInt, defaultFloat, defaultString, defaultBool) //дефолтное значение для стринг - ""
+	//return
+
+
 	//бинарные данные
 	var binary byte = '\x27'
 	println("binary:", binary)
@@ -41,13 +63,37 @@ func main() {
 	num := 42 //целый тип. Используемое имя переменной не должно быть объявлено ранее
 	str := "Мир"
 	println(num, str)
+	//return
+
+
+	//объявление нескольких переменных
+	var v1, v2 string = "v1", "v2"
+	println(v1, v2)
+	println(m1, m2, m3) //смотреть объявление в одном блоке var (вначале программы, где глобальные переменные)
+	//return
 
 
 	//приведение типов
-	println("float to int conversion")
+	var floatTMP float32 = 32.7
+	println("float to int conversion:", int(floatTMP)) //32
+	var unsignedTMP uint = 3
+	var signedTMP int = 11
+	println(signedTMP + int(unsignedTMP))
+	println("int to string conversation:", string(65)) //буква "A" по номеру в таблице юникода
+	//return
 
 
+	//конкатенация строк и длина строки
+	s1 := "Adam"
+	s2 := "Babam"
+	fullName := s1 + " " + s2
+	println(fullName, len(fullName)) //len - считает длину переданной строки
 
+	//ескейп сиволы в строках
+	escaping := `Hello\r\n //comment 
+	World` //используется при создании шаблонов и регулярных выражений
+	println(escaping) //\r\n //comment, перевод строки и табуляцию перед World выведет на экран
+	//return
 
 }
 
