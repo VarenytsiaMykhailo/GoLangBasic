fd66ef2a14c3a32acb79396e9e7f7f78b9555edd
VarenytsiaMykhailo <l2fonat@bk.ru>
2019-09-25 20:32:25 +0300

    added returnMultipleValuesFromFunction, updated basicSyntax

---
+++ returnMultipleValuesFromFunction.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	x, y, z := foo() //присвоение возвращаемых значений из функции foo
+	println(x, y, z) //100 false abraCodabra
+
+	//можно игнорировать некоторые возвращаемые значения:
+	_, d, _ := foo()
+	println(d) //false
+}
+
+func foo() (int, bool, string) {
+	return 100, false, "abraCodabra" //возвращаем несколько типов значений из функции
+}
