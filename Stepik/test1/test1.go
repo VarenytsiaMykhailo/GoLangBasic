b1c5606a15f7a5b95fc86fa4ecb413fa5a14c1a9
VarenytsiaMykhailo <l2fonat@bk.ru>
2019-10-05 02:23:04 +0300

    bigDigits

--- Stepik/test1/test1.go
+++ Stepik/test1/test1.go
@@ -3,26 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a, b, i int
-	fmt.Scan(&a, &b)
-	if (a / 1000 != 0){
-		i = 1000
-	} else if (a / 100 != 0){
-		i = 100
-	} else if (a / 10 != 0){
-		i = 10
-	}
 
-	fmt.Println(i)
-	/*for ; i >= 10; i = i / 10{
-		switch ((a / i) % 10) {
-		case 0: fmt.Println("Zero")
-		case 1: fmt.Println("One")
-		case 2: fmt.Println("Two")
-		case 3: fmt.Println("Three")
-		case 4: fmt.Println("Four")
-		case 5: fmt.Println("Five")
-		default: fmt.Println("Unknown Number")
-		}
-	}*/
+	str := "sd50"
+	fmt.Println(str[2])
 }
