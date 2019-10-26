0eef445144bda98f7c86c1af8817260bf920ef2e
VarenytsiaMykhailo <l2fonat@bk.ru>
2019-10-26 20:20:07 +0300

    Update polar2cartesian.go

--- MarkSummerfieldProgrammingInGO/polar2cartesian/polar2cartesian.go
+++ MarkSummerfieldProgrammingInGO/polar2cartesian/polar2cartesian.go
@@ -8,6 +8,16 @@ import (
 	"runtime" // юзаем для определения типа ОС
 )
 
+type polar struct {
+	radius float64
+	angle float64
+}
+
+type cartesian struct {
+	x float64
+	y float64
+}
+
 var prompt = "Enter a radius and an angle (in degrees), e.g.: 12.5 90  , or %s to quit."
 func init()  {
 	if runtime.GOOS == "windows" {
@@ -23,15 +33,41 @@ func main()  {
 	answers := createSolver(questions)
 	defer close(answers)
 	interact(questions, answers)
-
 }
 
-type polar struct {
-	radius float64
-	angle float64
+func createSolver(questions chan polar) chan cartesian {
+	answers := make(chan cartesian)
+	go func() {
+		for {
+			polarCoord := <- questions
+			angle := polarCoord.angle * math.Pi / 180.0 // преобразование градусов в радианы
+			x := polarCoord.radius * math.Cos(angle)
+			y := polarCoord.radius * math.Sin(angle)
+			answers <- cartesian{x, y}
+		}
+	}()
+	return answers
 }
 
-type cartesian struct {
-	x float64
-	y float64
+const result = "Polar radius=%.02f angle=%.02f° → Cartesian x=%.02f y=%.02f\n"
+
+func interact(questions chan polar, answers chan cartesian)  {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(prompt)
+	for {
+		fmt.Printf("Radius and angle: ")
+		line, err := reader.ReadString('\n') //читает строку, пока не дойдет до \n
+		if err != nil {
+			break
+		}
+		var radius, angle float64
+		if _, err := fmt.Sscanf(line, "%f %f", &radius, &angle); err != nil {
+			fmt.Fprintln(os.Stderr, "invalid input")
+			continue
+		}
+		questions <- polar{radius, angle}
+		coord := <- answers
+		fmt.Printf(result, radius, angle, coord.x, coord.y)
+	}
+	println()
 }
